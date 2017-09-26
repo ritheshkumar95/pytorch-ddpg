3bd39f923e5f6d4f840fe0e6b47f5e1a7b7b5c8b
ritheshkumar95 <ritheshkumar.95@gmail.com>
2017-09-26 09:36:24 -0400

    pushing config fixes

---
+++ config/baseline.py
@@ -0,0 +1,16 @@
+# rl parameters
+max_episodes = 5000
+max_steps = 1000
+max_buffer = 1000000
+
+# noise parameters
+mu = 0
+theta = 0.15
+sigma = 0.2
+
+actor_learning_rate = 1e-4
+critic_learning_rate = 1e-3
+batch_size = 64
+
+gamma = 0.99
+tau = 0.001
